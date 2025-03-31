2b59c064d138b1038448108a7d9f02a38c4584bb
Regina <125544191+RgBunn@users.noreply.github.com>
2025-03-31 19:40:36 +0800

    lesson8 classWork

---
+++ lesson_8/script.js
@@ -0,0 +1,47 @@
+// console.dir(document.body.style.backgroundColor="green");
+// console.log(document.body.children);
+// Array.from(document.body.children).forEach((element) => {
+//     console.log(element);
+// })
+// [...document.body.children].forEach((element) => {
+//     console.log(element);
+// })
+// Array.prototype.forEach.call(document.body.children, function(el) {
+//     console.log(el)
+// })
+// console.log(document.body.children[0].nextElementSibling.children[0].parentElement.lastElementChild);
+
+// console.log(document.getElementById('test'));
+// // console.log(document.getElementsByName())
+// // console.log(document.getElementsByTagName('footer'))
+// const footers=document.getElementsByTagName('footer');
+//
+// const footer=document.createElement('footer');
+// document.body.appendChild(footer);
+// console.log(footers);
+
+// const el=document.querySelector('div');
+// console.log(el.closest('div'));
+
+// document.body.style
+//////////////////////////////////////////
+// const colors=['red','green','blue','yellow','orange','red','blue','pink','purple','olive'];
+// setInterval(()=>{
+//     document.body.style.backgroundColor=colors[Math.floor(Math.random()*colors.length)];
+// },2000)
+//////////////////////////////////////////
+// document.body.id="world";
+// document.body.setAttribute('hello', 'world');
+// document.body.dataset.helloWorld=true;
+// document.body.classList.add("dark",'css');
+// document.body.classList.remove("dark");
+// document.body.classList.toggle("dark");
+
+// console.log(document.body.classList.contains("dark"));
+
+//////////////////////////////////////////
+
+//fill the gap
+
+//////////////////////////////////////////
+console.log(document.querySelector('h2').innerText='test');
