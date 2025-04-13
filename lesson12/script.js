2e34a5a659eea0bb296efd0caf268b56f6ea1520
Regina <125544191+RgBunn@users.noreply.github.com>
2025-04-13 09:07:39 +0800

    lesson12 classWork

---
+++ lesson12/script.js
@@ -0,0 +1,47 @@
+// console.log("Hello World!");
+//
+// fetch('https://jsonplaceholder.typicode.com/posts',{
+//     method: 'POST',
+//     body: JSON.stringify({
+//         hello: 'world',
+//     })
+// })
+// .then(res => {
+//     console.log(res.headers.get('content-type'));
+//     if(res.ok) return res.json();
+// })
+// .then(json => console.log(json))
+
+///////////////////////////////////
+// const controller =new AbortController();
+// // console.log(controller);
+// controller.signal.addEventListener('abort', ()=>{
+//     console.log('Canceled');
+// });
+// controller.abort();
+////////////////////////////////////////
+// fetch('https://jsonplaceholder.typicode.com/posts',{
+//     method: 'POST',
+//     signal:controller.signal,
+//     headers:{
+//         'Content-Type': 'application/json',
+//     }
+//     // body: JSON.stringify({
+//     //     hello: 'world',
+//     // })
+// })
+// .then(res => {
+//     console.log(res.headers.get('content-type'));
+//     if(res.ok) return res.json();
+// })
+// .then(json => console.log(json))
+//
+// controller.abort();
+////////////////////////////////////////
+// const url='https://www.google.com/search/search?q=kjdhgksl';
+//     fetch(url)
+//         .then(res => res.json())
+//         .then(data => console.log(data))
+
+const a="Hello Avito";
+console.log(a.split(' ')[1][0])
